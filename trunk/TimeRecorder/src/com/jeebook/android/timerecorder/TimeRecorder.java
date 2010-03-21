bf1c4a08b7aad8cd494ddce930819d77b3e83421
com.chinaos.snaill <com.chinaos.snaill@6ac803c6-2dd1-11df-9f25-831aa717a150>
2010-03-21 04:57:37 +0000

    修改兼容Android 1.5 修改配置界面采用Preference 添加主界面倒计时效果 添加放弃当前闹钟功能（未测试通过）

--- trunk/TimeRecorder/src/com/jeebook/android/timerecorder/TimeRecorder.java
+++ trunk/TimeRecorder/src/com/jeebook/android/timerecorder/TimeRecorder.java
@@ -1,16 +1,48 @@
 package com.jeebook.android.timerecorder;
 
 import java.util.*;
-import java.text.*;
 import android.app.*;
 import android.view.*;
 import android.widget.*;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.content.Context;
 import android.content.Intent; 
-import com.jeebook.android.*;
+import android.content.SharedPreferences;
 
 public class TimeRecorder extends Activity {
-    /** Called when the activity is first created. */
+	Timer _timer = new Timer();
+	Date _date = new Date();
+
+	Handler handler = new Handler() {
+        public void handleMessage(Message msg) {  
+            switch (msg.what) {      
+            	case 1: {
+            		long time = _date.getTime() - System.currentTimeMillis();
+            		if ( time < 0 )
+            		{	
+            			time = 0;
+            			_timer.cancel();
+            		}
+            		TextView messager = (TextView)findViewById(R.id.MessageView);
+                	messager.setText(String.format("%d ms", time));
+            		break;
+            	}
+            }      
+            super.handleMessage(msg);  
+        } 
+	};
+	
+	TimerTask task = new TimerTask(){  
+		  
+	        public void run() {  
+	            Message message = new Message();      
+	            message.what = 1;      
+	            handler.sendMessage(message);    
+	        }   
+	}; 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,38 +59,47 @@ public class TimeRecorder extends Activity {
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent();
-				intent.setClass(TimeRecorder.this, TimeRecorderOption.class);
-				startActivity(intent);
-				TimeRecorder.this.finish();
+				ShowOption();
 			}
 		});
         
         // 获取配置
-        AppConfig ac = new AppConfig(this, R.string.config_name);
-        ac.load();
-        if ( ac.isEmpty() )
+        String strInterval = getString(R.string.pref_key_interval);
+        String strAlarmTime = getString(R.string.pref_key_alarmtime);
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.config_name), 0);
+        if ( !preferences.contains(strInterval))
         {
-        	ac.set(R.string.op_interval_hour, 8);
-        	ac.set(R.string.op_interval_minute, 0);
-        	ac.save();
+        	ShowOption();
+        	return;
         }
-        int hour = ac.getInt(R.string.op_interval_hour);
-        int minute = ac.getInt(R.string.op_interval_minute);
         
-        // 计算时间
-        Date date = new Date();
-        date.setTime( date.getTime() + (hour * 60 + minute) * 60 * 1000 );
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        TextView msg = (TextView)findViewById(R.id.MessageView);
-        msg.setText(sdf.format(date));
+        long atime = preferences.getLong(strAlarmTime, 0);
+        if ( atime != 0 )
+        {
+        	_date.setTime(atime);
+        }
+        else
+        {
+        	String interval = preferences.getString(strInterval, "0:00");
+        	int hour = Integer.valueOf(interval.split(":")[0]);
+        	int minute = Integer.valueOf(interval.split(":")[1]);
         
-        // 设置提醒 
-        Intent intent = new Intent(this, AlarmReceiver.class);  
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);  
-        AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-//        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+5000, pendingIntent);
-        am.set(AlarmManager.RTC_WAKEUP, date.getTime(), pendingIntent);
+        	// 计算时间
+        	_date.setTime( _date.getTime() + (hour * 60 + minute) * 60 * 1000 );
+        
+        	// 设置提醒 
+        	StartAlarm(this, _date);
+        	
+        	// 保存提醒
+           	SharedPreferences.Editor edit = preferences.edit();
+            edit.putLong(strAlarmTime, _date.getTime());
+            edit.commit();
+        }
+        
+        // 倒计时
+        java.util.Random r=new java.util.Random();
+        int rnd = ( r.nextInt() >>> 1 ) % 100;
+        _timer.scheduleAtFixedRate(task, rnd, rnd);
     }
     
     public boolean onTouchEvent (MotionEvent event)
@@ -67,4 +108,28 @@ public class TimeRecorder extends Activity {
     	return true;
     }
    
+    public void ShowOption()
+    {
+		Intent intent = new Intent();
+		intent.setClass(TimeRecorder.this, TimeRecorderOption.class);
+		startActivity(intent);
+		finish();
+    }
+    
+    public static void StartAlarm(Context context, Date date)
+    {
+        Intent intent = new Intent(context, AlarmReceiver.class);  
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);  
+        AlarmManager am = (AlarmManager)context.getSystemService(ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+5000, pendingIntent);
+//        am.set(AlarmManager.RTC_WAKEUP, date.getTime(), pendingIntent); 	
+    }
+    
+    public static void CancelAlarm(Context context)
+    {
+        Intent intent = new Intent(context, AlarmReceiver.class);  
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);  
+        AlarmManager am = (AlarmManager)context.getSystemService(ALARM_SERVICE);
+        am.cancel(pendingIntent);
+    }    
 }
