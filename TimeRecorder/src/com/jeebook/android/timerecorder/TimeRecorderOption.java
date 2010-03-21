ee72a2a4e97c90ee5076f1d63a05284d14ce10d7
com.chinaos.snaill <com.chinaos.snaill@6ac803c6-2dd1-11df-9f25-831aa717a150>
2010-03-21 04:57:37 +0000

    修改兼容Android 1.5 修改配置界面采用Preference 添加主界面倒计时效果 添加放弃当前闹钟功能（未测试通过）

--- TimeRecorder/src/com/jeebook/android/timerecorder/TimeRecorderOption.java
+++ TimeRecorder/src/com/jeebook/android/timerecorder/TimeRecorderOption.java
@@ -1,36 +1,75 @@
 package com.jeebook.android.timerecorder;
 
-import com.jeebook.android.AppConfig;
-import android.app.*;
-import android.widget.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.*;
 
-public class TimeRecorderOption extends Activity {
+public class TimeRecorderOption extends PreferenceActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.option);
+        addPreferencesFromResource(R.xml.preferences);
         
-        AppConfig ac = new AppConfig(this, R.string.config_name);
-        ac.load();
-        int hour = ac.getInt(R.string.op_interval_hour);
-        int minute = ac.getInt(R.string.op_interval_minute);
+        // 初始化配置
+        String strInterval = getString(R.string.pref_key_interval);
+        String strAlarmTime = getString(R.string.pref_key_alarmtime);
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.config_name), 0);
+        if ( !preferences.contains(strInterval))
+        {
+        	SharedPreferences.Editor edit = preferences.edit();
+        	edit.putString(strInterval, "8:00");
+        	edit.putLong(strAlarmTime, 0);
+        	edit.commit();
+        }
+        		
+        // 初始化配置界面
+        Preference alarmtime = getPreferenceScreen().findPreference(strAlarmTime);
+        long atime = preferences.getLong(strAlarmTime, 0);
+        if ( atime == 0 )
+        {
+        	alarmtime.setDefaultValue( false );
+        	alarmtime.setEnabled(false);
+        }
+        else
+        {
+        	alarmtime.setDefaultValue( true );
+        	Date date = new Date(atime);
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        	alarmtime.setSummary(sdf.format(date));
+        	alarmtime.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+				
+				@Override
+				public boolean onPreferenceChange(Preference preference, Object newValue) {
+					// TODO Auto-generated method stub
+					preference.setEnabled(false);
+					SharedPreferences.Editor edit = preference.getEditor();
+					edit.putLong(getString(R.string.pref_key_alarmtime), 0);
+					edit.commit();
+					
+					TimeRecorder.CancelAlarm(TimeRecorderOption.this);
+					return false;
+				}
+			});
+        }
         
-        TimePicker tp = (TimePicker)findViewById(R.id.TimePicker01);
-        tp.setIs24HourView(true);
-        tp.setCurrentHour(hour);
-        tp.setCurrentMinute(minute);
-    }
-    
-    @Override
-    public void onPause() {
-    	super.onPause();
-    	
-        TimePicker tp = (TimePicker)findViewById(R.id.TimePicker01);
-        AppConfig ac = new AppConfig(this, R.string.config_name);
-        ac.set(R.string.op_interval_hour, tp.getCurrentHour());
-    	ac.set(R.string.op_interval_minute, tp.getCurrentMinute());
-    	ac.save(); 
+        Preference interval = getPreferenceScreen().findPreference(strInterval);
+        interval.setDefaultValue(preferences.getString(strInterval, "Error"));
+        interval.setSummary(preferences.getString(strInterval, "Error"));
+        interval.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+			
+			@Override
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				// TODO Auto-generated method stub
+				preference.setSummary(newValue.toString());
+				
+				SharedPreferences.Editor edit = preference.getEditor();
+				edit.putString(getString(R.string.pref_key_interval), newValue.toString());
+				edit.commit();
+				return true;
+			}
+		});
     }
 }
