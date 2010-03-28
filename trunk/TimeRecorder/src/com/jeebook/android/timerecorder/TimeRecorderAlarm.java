098278065aef1da5f6a8fec3813b8645b518c206
com.chinaos.snaill <com.chinaos.snaill@6ac803c6-2dd1-11df-9f25-831aa717a150>
2010-03-28 11:02:56 +0000

    增加释放Timer代码 增加各界面的在界面切换时释放的代码 修改Alarm触发时没有清除闹铃的BUG 升级到1.6版

--- trunk/TimeRecorder/src/com/jeebook/android/timerecorder/TimeRecorderAlarm.java
+++ trunk/TimeRecorder/src/com/jeebook/android/timerecorder/TimeRecorderAlarm.java
@@ -1,14 +1,17 @@
 package com.jeebook.android.timerecorder;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MotionEvent;
 import android.view.Window;
 
 public class TimeRecorderAlarm extends Activity {
     /** Called when the activity is first created. */
 	private MediaPlayer _mp;
+	private boolean _finish = false;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,29 @@ public class TimeRecorderAlarm extends Activity {
     
     public boolean onTouchEvent (MotionEvent event)
     {
+    	finishAlarm();
+    	return true;
+    }
+    
+    public void onPause() {
+    	super.onPause();
+    	
+    	if ( !_finish )
+    		finishAlarm();
+    }
+    
+    public void finishAlarm() {
+    	//
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor edit = preferences.edit();
+		edit.putLong(getString(R.string.pref_key_alarmtime), 0);
+		edit.commit();
+		
+		//
     	_mp.stop();
     	_mp.release();
     	finish();
-    	return true;
+    	
+    	_finish = true;
     }
 }
