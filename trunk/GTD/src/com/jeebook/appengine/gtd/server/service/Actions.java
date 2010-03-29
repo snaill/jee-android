414a19023d23f909bbc1bc56ff79dae5a688d08c
com.chinaos.snaill <com.chinaos.snaill@6ac803c6-2dd1-11df-9f25-831aa717a150>
2010-03-29 15:03:04 +0000

    添加Servlet

---
+++ trunk/GTD/src/com/jeebook/appengine/gtd/server/service/Actions.java
@@ -0,0 +1,12 @@
+package com.jeebook.appengine.gtd.server.service;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Actions extends HttpServlet {
+	protected  void	doGet(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+}
