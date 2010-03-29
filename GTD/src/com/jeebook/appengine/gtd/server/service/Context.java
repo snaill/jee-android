13d866c9d1f8604d955c2fb65364b03cb6e349a2
com.chinaos.snaill <com.chinaos.snaill@6ac803c6-2dd1-11df-9f25-831aa717a150>
2010-03-29 15:03:04 +0000

    添加Servlet

---
+++ GTD/src/com/jeebook/appengine/gtd/server/service/Context.java
@@ -0,0 +1,27 @@
+package com.jeebook.appengine.gtd.server.service;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Context extends HttpServlet {
+	protected  void	doGet(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+	
+	protected  void	doPost(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+	
+	protected  void	doDelete(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+		
+	protected  void	doPut(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+}
