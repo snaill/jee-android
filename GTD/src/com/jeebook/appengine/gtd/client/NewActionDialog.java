3188c7b9d767ce293fa283617a84486948555b02
com.chinaos.snaill <com.chinaos.snaill@6ac803c6-2dd1-11df-9f25-831aa717a150>
2010-05-06 09:16:00 +0000

    修改NewAction界面 添加NewProject界面 添加NewContext界面

---
+++ GTD/src/com/jeebook/appengine/gtd/client/NewActionDialog.java
@@ -0,0 +1,52 @@
+package com.jeebook.appengine.gtd.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
+
+public class NewActionDialog extends DialogBox {
+
+	private static NewActionDialogUiBinder uiBinder = GWT
+			.create(NewActionDialogUiBinder.class);
+
+	interface NewActionDialogUiBinder extends UiBinder<Widget, NewActionDialog> {
+	}
+
+	public NewActionDialog() {
+	    // Use this opportunity to set the dialog's caption.
+	    setText("About the Mail Sample");
+	    setWidget(uiBinder.createAndBindUi(this));
+
+	    setAnimationEnabled(true);
+	    setGlassEnabled(true);
+	}
+
+	  @Override
+	  protected void onPreviewNativeEvent(NativePreviewEvent preview) {
+	    super.onPreviewNativeEvent(preview);
+
+	    NativeEvent evt = preview.getNativeEvent();
+	    if (evt.getType().equals("keydown")) {
+	      // Use the popup's key preview hooks to close the dialog when either
+	      // enter or escape is pressed.
+	      switch (evt.getKeyCode()) {
+	        case KeyCodes.KEY_ENTER:
+	        case KeyCodes.KEY_ESCAPE:
+	          hide();
+	          break;
+	      }
+	    }
+	  }
+
+	  @UiHandler("closeButton")
+	  void onSignOutClicked(ClickEvent event) {
+	    hide();
+	  }
+
+}
--- GTD/src/com/jeebook/appengine/gtd/client/TopPanel.java
+++ GTD/src/com/jeebook/appengine/gtd/client/TopPanel.java
@@ -16,14 +16,20 @@ package com.jeebook.appengine.gtd.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.Button;
 import com.google.inject.Inject;
+import com.jeebook.appengine.gtd.client.InboxPanel.InboxPanelUiBinder;
 import com.jeebook.appengine.gtd.client.model.LoginInfo;
 import com.jeebook.appengine.gtd.client.service.LoginServiceAsync;
 
@@ -32,8 +38,10 @@ import com.jeebook.appengine.gtd.client.service.LoginServiceAsync;
  */
 public class TopPanel extends Composite {
 
-  interface Binder extends UiBinder<Widget, TopPanel> { }
-  private static final Binder binder = GWT.create(Binder.class);
+	private static TopPanelUiBinder uiBinder = GWT.create(TopPanelUiBinder.class);
+
+	interface TopPanelUiBinder extends UiBinder<Widget, TopPanel> {
+	}
 
   @Inject ShuffleConstants constants;
   
@@ -41,39 +49,27 @@ public class TopPanel extends Composite {
   @UiField Anchor loginLink;
   @UiField Anchor settingsLink;
   @UiField Anchor newActionLink;
+  @UiField Anchor newProjectLink;
+  @UiField Anchor newContextLink;
   @UiField Anchor reportLink;
   
   @Inject
   public TopPanel(LoginServiceAsync loginService, final LoginInfo loginInfo) {
-    initWidget(binder.createAndBindUi(this));
-    
-    loginService.login(getModuleUrl(), new AsyncCallback<LoginInfo>() {
-        public void onFailure(Throwable error) {
-        }
-
-        public void onSuccess(LoginInfo result) {
-          loginInfo.populate(result);
-          if(loginInfo.isLoggedIn()) {
-              loginLink.setText(constants.signOut());
-              loginLink.setHref(loginInfo.getLogoutUrl());
-              emailSpan.setInnerText(loginInfo.getEmailAddress());
-          } else {
-              loginLink.setText(constants.signIn());
-              loginLink.setHref(loginInfo.getLoginUrl());
-              emailSpan.setInnerText("");
-          }
-        }
-      });
+    initWidget(uiBinder.createAndBindUi(this));
   }
+
+  @UiHandler("newActionLink")
+	void onNewActionClick(ClickEvent e) {
+			new NewActionDialog().show();
+	}
   
-  private String getModuleUrl() {
-      String url = Location.getPath();
-      String gwtCodeSvrParam = Location.getParameter("gwt.codesvr");
-      if (!"".equals(gwtCodeSvrParam)) {
-        url += "?gwt.codesvr=" + gwtCodeSvrParam;
-      }     
-      
-      return url;
-  }
-
+  @UiHandler("newProjectLink")
+	void onNewProjectClick(ClickEvent e) {
+			new NewProjectDialog().show();
+	}
+  
+  @UiHandler("newContextLink")
+	void onNewContextClick(ClickEvent e) {
+			new NewContextDialog().show();
+	}
 }
