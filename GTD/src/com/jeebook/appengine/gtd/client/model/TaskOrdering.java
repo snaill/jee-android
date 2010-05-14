2fc6b4fe202bfd0e125717e784f35ce0e763b263
com.chinaos.snaill <com.chinaos.snaill@6ac803c6-2dd1-11df-9f25-831aa717a150>
2010-05-14 14:20:33 +0000

    清除未使用的旧文件及目录

--- GTD/src/com/jeebook/appengine/gtd/client/model/TaskOrdering.java
+++
@@ -1,8 +0,0 @@
-package com.jeebook.appengine.gtd.client.model;
-
-import java.io.Serializable;
-
-@SuppressWarnings("serial")
-public class TaskOrdering implements Serializable {
-
-}
--- GTD/src/com/jeebook/appengine/gtd/client/service/LoginServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package com.jeebook.appengine.gtd.client.service;
-
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.jeebook.appengine.gtd.client.model.LoginInfo;
-
-public interface LoginServiceAsync {
-  public void login(String requestUri, AsyncCallback<LoginInfo> async);
-}
